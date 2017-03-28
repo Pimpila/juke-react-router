32c43fc8b078585d1e0dbecfb4a31056c5b489df
Chloe Choi <chloe.meowc@gmail.com>
2017-03-28 18:35:40 -0400

    first!!!

--- browser/react/components/Album.js
+++ browser/react/components/Album.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React { Component } from 'react';
 import Songs from '../components/Songs';
 
-const Album = (props) => {
+class Album extends Component {
 
-  const album = props.album;
-  const currentSong = props.currentSong;
-  const isPlaying = props.isPlaying;
-  const toggleOne = props.toggleOne;
+    constructor(props){
+      super(props);
+      this.state = {};
 
-  return (
-    <div className="album">
-      <div>
-        <h3>{ album.name }</h3>
-        <img src={ album.imageUrl } className="img-thumbnail" />
-      </div>
-      <Songs
-        songs={album.songs}
-        currentSong={currentSong}
-        isPlaying={isPlaying}
-        toggleOne={toggleOne} />
-    </div>
-  );
+    }
+      // const album = this.props.album;
+      // const currentSong = this.props.currentSong;
+      // const isPlaying = this.props.isPlaying;
+      // const toggleOne = this.props.toggleOne;
+      //
+    render() {
+        return (
+            <div className="album">
+                <div>
+                    <h3>{ album.name }</h3>
+                      <img src={ album.imageUrl } className="img-thumbnail"/>
+                  </div>
+                  <Songs
+                      songs={album.songs}
+                      currentSong={currentSong}
+                      isPlaying={isPlaying}
+                      toggleOne={toggleOne}/>
+              </div>
+          );
+      }
 }
 
 export default Album;
